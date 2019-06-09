4444cdb6f1db664f8205846ea33bf97d0f1efb8a
KT_Zheng <a6163508@gmail.com>
2019-06-09 11:33:26 +0800

    顯示餐點，增加餐點完成。

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,109 @@
 //
 // TODO(you): Add the JavaScript necessary to complete your final project.
 //
+class App {
+  constructor() {
+    this.menu = new Menu();
+    this.fetchApi = new FetchApi();
+
+    this.fetchApi.getData(this.menu.render);
+    
+    this.addBtn = document.getElementsByClassName('add-btn')[0];
+
+    this.addBtn.addEventListener('click', () => {
+      const addScreen = document.getElementById('add');
+      const clone = document.importNode(addScreen.content, true);
+
+      document.body.appendChild(clone);
+      const target = document.getElementsByClassName('add-screen')[0];
+
+      document.getElementById('add-btn').addEventListener('click', (event) => {
+        event.preventDefault();
+        const data = {};
+        data['food-name'] = document.getElementById('food-name').value;
+        data['food-img'] = document.getElementById('food-img').value;
+        data['food-price'] = document.getElementById('food-price').value;
+        this.fetchApi.postData(data);
+        document.body.removeChild(target);
+        console.log('add sucess');
+      });
+      document.getElementById('exit-btn').addEventListener('click', (event) => {
+        event.preventDefault();
+        document.body.removeChild(target);
+        console.log('exit sucess');
+      });
+    });
+  }
+}
+
+class Menu {
+  constructor() {
+    this.menu = document.getElementsByClassName('menu')[0];
+    this.foodTemplate = document.getElementById('food');
+    this.render = this.render.bind(this);
+  }
+
+  render(data) {
+    while (this.menu.firstChild) {
+      this.menu.removeChild(this.menu.firstChild);
+    }
+
+    data.forEach((food, index) => {
+      const template = document.importNode(this.foodTemplate.content, true);
+      this.menu.appendChild(template);
+      const foodName = document.getElementsByClassName('food-name')[index];
+      const foodImg = document.getElementsByClassName('food-img')[index];
+      const foodPrice = document.getElementsByClassName('food-price')[index];
+
+      foodName.textContent = food['food-name'];
+      foodImg.src = food['food-img'];
+      foodPrice.textContent = food['food-price'];
+    });
+  }
+}
+
+class FetchApi {
+  constructor() {
+    this.url = '/food';
+  }
+
+  async getData(render) {
+    try {
+      const response = await fetch(this.url);
+  
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        render(jsonResponse);
+        console.log(jsonResponse);
+      } else {
+        throw new Error('Oh my god!');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async postData(data) {
+    try {
+      const response = await fetch(this.url, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+  
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        console.log(jsonResponse);
+      } else {
+        throw new Error('Oh my god!');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+const app = new App();

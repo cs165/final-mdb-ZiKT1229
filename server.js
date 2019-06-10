5e5987f26abd7f41507d1521ca4742c1f3664765
KT_Zheng <a6163508@gmail.com>
2019-06-10 10:40:06 +0800

    server api

--- server.js
+++ server.js
@@ -8,19 +8,25 @@ const app = express();
 const jsonParser = bodyParser.json();
 
 app.use(express.static('public'));
+app.use(jsonParser);
 
 let db = null;
+let dbo = null;
 async function main() {
-  const DATABASE_NAME = 'cs193x-db';
-  const MONGO_URL = `mongodb://localhost:27017/${DATABASE_NAME}`;
+  const DB_USER = 'ZiKT1229';
+  const DB_PASSWORD = 'CCU-10097';
+  const DATABASE_NAME = 'final-mdb';
+  const MONGO_URL = `mongodb://${DB_USER}:${DB_PASSWORD}@ds131737.mlab.com:31737/${DATABASE_NAME}`;
 
   // The "process.env.MONGODB_URI" is needed to work with Heroku.
   db = await MongoClient.connect(process.env.MONGODB_URI || MONGO_URL);
+  dbo = db.db(DATABASE_NAME);
 
   // The "process.env.PORT" is needed to work with Heroku.
   const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(`Server listening on port ${port}!`);
+  console.log(`http://localhost:${port}/`);
 };
 
 main();
@@ -28,3 +34,19 @@ main();
 ////////////////////////////////////////////////////////////////////////////////
 
 // TODO(you): Add at least 1 GET route and 1 POST route.
+app.get('/food', (req, res) => {
+  dbo.collection('food').find().toArray((err, result) => {
+    if (err) return console.log(err);
+    console.log('Get sucess!');
+    res.send(JSON.stringify(result));
+  });
+});
+
+app.post('/food', (req, res) => {
+  console.log(req.body);
+  dbo.collection('food').insertOne(req.body, (err, result) => {
+    if (err) return console.log(err);
+    console.log('Post sucess!');
+    res.send(req.body);
+  });
+});
